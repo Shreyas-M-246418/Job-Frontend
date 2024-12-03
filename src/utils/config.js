29d2bfacaf174ae3182c5a40e1ddf8e217ebc134
Shreyas M <shreyas.m246418@gmail.com>
2024-12-03 12:06:32 +0530

    file change

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const GITHUB_CLIENT_ID = 'Ov23liqZEOFu8NrwVmWQ';
 export const API_BASE_URL = 'https://job-backend-o4oa.onrender.com';
 export const GITHUB_REPO_URL = 'https://api.github.com/repos/Shreyas-M-246418/job-backend';
-export const GITHUB_CALLBACK_URL = 'https://job-backend-o4oa.onrender.com/auth/github/callback';
+export const GITHUB_CALLBACK_URL = 'https://shreyas-m-246418.github.io/Job-frontend/#/auth/github/callback';
