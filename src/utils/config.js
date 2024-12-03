56db506306c8a15372f65991fb914f71f4732cec
Shreyas M <shreyas.m246418@gmail.com>
2024-12-03 11:54:51 +0530

    Initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,4 @@
+export const GITHUB_CLIENT_ID = 'Ov23liqZEOFu8NrwVmWQ';
+export const API_BASE_URL = 'https://job-backend-o4oa.onrender.com';
+export const GITHUB_REPO_URL = 'https://api.github.com/repos/Shreyas-M-246418/job-backend';
+export const GITHUB_CALLBACK_URL = 'https://job-backend-o4oa.onrender.com/auth/github/callback';
